40af535732e6525240f48a01b38794b90bf8e393
OnyeEbube <okontaonyeka@gmail.com>
2023-07-13 14:56:59 +0100

    Removing whitespaces

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -7,7 +7,6 @@
 --(= default will be the first element of the enumeration, here US)
 --If the table already exists, your script should not fail
 --Your script can be executed on any database
-
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
