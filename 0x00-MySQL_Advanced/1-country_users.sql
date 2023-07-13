885ecc418a047f8eace14a1b38fda80af17ff4a5
OnyeEbube <okontaonyeka@gmail.com>
2023-07-13 15:03:55 +0100

    checking something

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,4 +1,4 @@
---An SQL script that creates a table users following these requirements
+--An SQL script that creates a table users
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
