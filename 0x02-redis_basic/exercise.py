86d9224b8923a8c874978efaf0e59f63f3159536
OnyeEbube <okontaonyeka@gmail.com>
2023-07-20 10:26:32 +0100

    Adding corrections

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -42,8 +42,8 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable = None) ->\
-            Union[str, bytes, int, float]:
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, bytes, int, float]:
         data = self._redis.get(key)
         if data is None:
             return None
